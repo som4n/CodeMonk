d4469106cafabccc2247033b90615e27dab1d94e
som4n <61178177+som4n@users.noreply.github.com>
2024-06-16 03:12:47 +0530

    style.js
    Basic styles

--- flims/src/components/App.jsx
+++ flims/src/components/App.jsx
@@ -3,20 +3,29 @@ import { CssBaseline } from '@mui/material';
 import {  Routes, Route } from 'react-router-dom';
 import { Actors, Movieinfo, Movies, Navbar, Profile } from './index';
 
-const App = () => (
-  <div>
-    <Navbar />
-    <CssBaseline />
-    <main>
-      
-        <Routes>
-          <Route path="/" element={<Movies />} />
-          <Route path="/actors/" element={<Actors />} />
-          <Route path="/movie/:id" element={<Movieinfo />} />
-          <Route path="/profile/:id" element={<Profile />} />
-        </Routes>
-    </main>
-  </div>
-);
+import useStyles from './styles';
+const App = () => {
+
+    const Classes = useStyles();
+
+    return (
+        <div className={Classes.root}>
+
+          <Navbar />
+          <CssBaseline />
+          <main className={Classes.content}>
+            
+            <div className={Classes.toolbar} />
+
+              <Routes>
+                <Route path="/" element={<Movies />} />
+                <Route path="/actors/" element={<Actors />} />
+                <Route path="/movie/:id" element={<Movieinfo />} />
+                <Route path="/profile/:id" element={<Profile />} />
+              </Routes>
+          </main>
+        </div>
+      )
+};
 
 export default App;
