96bac95e4e8393a618b6aa717996199f306187f2
som4n <61178177+som4n@users.noreply.github.com>
2024-06-16 04:02:44 +0530

    NavBar
    Theme changes and NavBar page

--- flims/src/components/Navbar/Navbar.jsx
+++ flims/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 
+import { AppBar,IconButton,Toolbar,Drawer,Button,Avatar, useMediaQuery } from "@mui/material";
+import { Menu, AccountCircle, Brightness4,Brightness7} from '@mui/icons-material';
+import { Link } from 'react-router-dom'
+import useStyles from './styles'
 const Navbar =() => {
-    console.log ('Navbar');
+    const Classes = useStyles();
+    const isMobile = useMediaQuery('(max-width:600px)')
     return (
-        <div>
-            <h1>Navbar</h1>
-        </div>
+
+        <AppBar position="fixed">
+            <Toolbar className={Classes.toolbar}>
+                {isMobile && (
+                    <IconButton 
+                    color="inherit" 
+                    edge='start'
+                    style={{outline:'none'}}
+                    onClick={() =>{}}
+                    className={Classes.menuButton}
+                    >
+                    <Menu/>
+                    </IconButton>
+                )}
+            </Toolbar>
+        </AppBar>
+   
     );
 };
 
